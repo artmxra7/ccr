65f1985227a542d0bfbba53644886a8c7613a4c8
Erwin Rahayu <artmxarea@gmail.com>
2019-12-02 09:21:03 +0700

    test

--- resources/views/layouts/homepage/mainlayout.blade.php
+++ resources/views/layouts/homepage/mainlayout.blade.php
@@ -1,7 +1,6 @@
 
     @include('layouts.homepage.nav')
     @include('layouts.homepage.header')
-    @include('layouts.homepage.herotop')
     @include('layouts.homepage.artikel')
     @include('layouts.homepage.complaintbox')
     @include('layouts.homepage.footer')
