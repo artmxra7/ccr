d648d723680ec325994cd99e0378e42cd8487d88
Erwin Rahayu <artmxarea@gmail.com>
2019-12-31 01:56:43 +0700

    update

--- resources/views/artikel/kampus/list.blade.php
+++ resources/views/artikel/kampus/list.blade.php
@@ -1,5 +1,13 @@
 
-  @include('layouts.homepage.nav')
+
+
+@if (Auth::guest())
+@include('layouts.homepage.nav_guest')
+@else
+
+@include('layouts.homepage.nav')
+@endif
   @include('layouts.homepage.header_user')
+
   <div class="jumbotron jumbotron-gradient"></div>
   @include('artikel.bumn.content_artikel')
